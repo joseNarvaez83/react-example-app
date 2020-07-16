f0cc6b6730e81d6b336cf7bc5801071f0d0b0615
Jose Narvaez <jose.narvaez.ie@gmail.com>
2020-07-16 15:54:06 -0500

    Enabled css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 //import styled from 'styled-components';
 import Person from './Person/Person';
 
@@ -86,6 +86,7 @@ class App extends Component {
     };*/
 
     let persons = null;
+    let btnClass = '';
 
     if ( this.state.showPersons ) {
       persons = (
@@ -102,32 +103,27 @@ class App extends Component {
         </div>
       );
 
-      /*style.backgroundColor = 'red';
-      style[":hover"] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }*/
+      btnClass = classes.Red;
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
 
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className='App'>
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
-        <button /*alt={this.state.showPersons}*/ className='button' onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button  className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle persons
         </button>
         {persons}
       </div>
-      //React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Test React createElement!'))
     );
   }
 }
